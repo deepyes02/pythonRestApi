e3a28cb4d1e09008aeecfd46e2a5f398ab8aee8b
deepyes02 <deepyes@outlook.com>
2020-07-01 11:50:38 +0545

    A simple restful api with python, django and djangorestframework

---
+++ myapi/urls.py
@@ -0,0 +1,12 @@
+##this file was created following rules from urls.py in project folder
+from django.urls import include,path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'heroes', views.HeroViewSet)
+
+urlpatterns = [
+  path('', include(router.urls)),
+  path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
